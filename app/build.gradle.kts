8bf5735f8548b5fc66fdada408230fc664a0fe46
agulia <keiji20000315@gmail.com>
2026-07-17 19:41:50 +0900

    古いアンドロイドに対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.kakeibo_compose"
-        minSdk = 35
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
